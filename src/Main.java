f8dffa2718d1f6be1be22346846be6540e06a27b
Khorezmiy1991 <khamza@inbox.ru>
2020-10-25 16:58:18 +0500

    Added Main class

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1. Regions massivini ochish - 20 ta joy bo'lsin - boshlanishiga 3ta qiymat berilsin
+
+        // 2.Distric massivini ochish - 20 ta joy bo'lsin - boshlanishiga 5ta qiymat berilsin
+
+        // 3. User massivini ochish - 20 ta joy bo'lsin - boshlanishiga 3ta qiymat berilsin
+
+        // 4. Category massivini ochish - 20 ta joy bo'lsin - boshlanishiga 3ta qiymat berilsin
+
+        // 5. SubCategory massivini ochish - 20 ta joy bo'lsin - boshlanishiga 6ta qiymat berilsin
+
+        // 6. Message massivini ochish - 20 ta joy bo'lsin
+        // 7. Announcement massivini ochish - 20 ta joy bo'lsin
+
+
+        // printMainMenu - method
+        // login
+        // signUp
+        // Exit
+    }
+}
