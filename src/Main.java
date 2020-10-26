c18588a00aadf901790bd0c835d44a46bd2e45b7
zukhranazarova <nazarovazimjon8@gmail.com>
2020-10-26 10:29:13 +0500

    Added write message and inbox

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     static Scanner scanner = new Scanner(System.in);
     static User onlineUser = null;
+
     public static void main(String[] args) {
 
         int choice;
@@ -26,36 +27,34 @@ public class Main {
         // Exit
 
         Region[] regions = new Region[20];
-        regions[0] = new Region(1,"Toshkent");
-        regions[1] =new Region(2, "Samarqand");
-        regions[2] =new Region(3,"Xorazm");
+        regions[0] = new Region(1, "Toshkent");
+        regions[1] = new Region(2, "Samarqand");
+        regions[2] = new Region(3, "Xorazm");
 
         District[] districts = new District[20];
-        districts[0] =new District(1,"Yunusobod",regions[0]);
-        districts[1] =new District(2, "Urganch",regions[2]);
-        districts[2] =new District(3,"Urgut",regions[1]);
-        districts[3] =new District(4, "Chilonzor",regions[0]);
-        districts[4] =new District(5, "Xiva",regions[2]);
+        districts[0] = new District(1, "Yunusobod", regions[0]);
+        districts[1] = new District(2, "Urganch", regions[2]);
+        districts[2] = new District(3, "Urgut", regions[1]);
+        districts[3] = new District(4, "Chilonzor", regions[0]);
+        districts[4] = new District(5, "Xiva", regions[2]);
 
-        User[]users=new User[20];
-        users[0]=new User("Anvar","anvarganiev@.uz",districts[0],"123","994567834");
-        users[1]=new User("Zarina","usmonovazarina@.ru",districts[1],"1234","974563245");
-        users[2]=new User("Tolibjon","tolibkarimov@gmail.com",districts[2],"12345","904562345");
-
-
-        Category[] categories=new Category[20];
-        categories[0]=new Category(1,"Transport","qwerty");
-        categories[1]=new Category(2,"Bolalar dunyosi","rtyu");
-        categories[2]=new Category(3,"Elektronika","fhgty");
+        User[] users = new User[20];
+        users[0] = new User("Anvar", "anvarganiev@.uz", districts[0], "123", "994567834");
+        users[1] = new User("Zarina", "usmonovazarina@.ru", districts[1], "1234", "974563245");
+        users[2] = new User("Tolibjon", "tolibkarimov@gmail.com", districts[2], "12345", "904562345");
 
 
+        Category[] categories = new Category[20];
+        categories[0] = new Category(1, "Transport", "qwerty");
+        categories[1] = new Category(2, "Bolalar dunyosi", "rtyu");
+        categories[2] = new Category(3, "Elektronika", "fhgty");
 
-        SubCategory[] subcategories=new SubCategory[20];
-        subcategories[0]=new SubCategory(1,"Televizor",categories[2]);
-        subcategories[1]=new SubCategory(2,"Avto",categories[0]);
-        subcategories[2]=new SubCategory(3,"Bolalar kiyimlari",categories[1]);
-        subcategories[3]=new SubCategory(4,"Telefon",categories[2]);
 
+        SubCategory[] subcategories = new SubCategory[20];
+        subcategories[0] = new SubCategory(1, "Televizor", categories[2]);
+        subcategories[1] = new SubCategory(2, "Avto", categories[0]);
+        subcategories[2] = new SubCategory(3, "Bolalar kiyimlari", categories[1]);
+        subcategories[3] = new SubCategory(4, "Telefon", categories[2]);
 
 
         Message[] messages = new Message[20];
@@ -235,5 +234,111 @@ public class Main {
 
         System.out.println("-----------------------------------------");
     }
+    private static void writeMessage(Announcement[] announcements, Message[] messages) {
+
+        if (hasAnnouncement) {
+            viewAnnouncements(announcements);
+
+            System.out.println("Choose the announcement:");
+            int choice = scanner.nextInt();
+            Announcement announcement = announcements[choice - 1];
+
+            System.out.println();
+            System.out.println(announcement.getTitle() + " | " + announcement.getUser().getName()
+                    + " | " + announcement.getDistrict().getName() + ", " + announcement.getDistrict().getRegion().getName());
+
+            System.out.println("New Message");
+            System.out.print("Title: ");
+
+            scanner = new Scanner(System.in);
+            String title = scanner.nextLine();
+            System.out.print("Body: ");
+            String body = scanner.nextLine();
+
+            messages[indexMessage++] = new Message(title, body, onlineUser, announcement);
+            System.out.println("Message sent!");
+        } else {
+            System.out.println("There is no announcement!");
+        }
+    }
+
+    private static void viewInbox(Message[] messages) {
+        int index = 1;
+        System.out.println("All Messages");
+        System.out.println("-----------------------------------------");
+        for (Message message : messages) {
+            if (message != null) {
+                if (message.getAnnouncement().getUser().equals(onlineUser)) {
+                    System.out.println(index + ". " + message.toString());
+                    index++;
+                }
+            }
+        }
+        if (index == 1)
+            System.out.println("No data!");
+        System.out.println("-----------------------------------------");
+    }
+
+    private static void writeMessage(Announcement[] announcements, Message[] messages) {
+
+        if (hasAnnouncement) {
+            viewAnnouncements(announcements);
+
+            System.out.println("Choose the announcement:");
+            int choice = scanner.nextInt();
+            Announcement announcement = announcements[choice - 1];
+
+            System.out.println();
+            System.out.println(announcement.getTitle() + " | " + announcement.getUser().getName()
+                    + " | " + announcement.getDistrict().getName() + ", " + announcement.getDistrict().getRegion().getName());
+
+            System.out.println("New Message");
+            System.out.print("Title: ");
+
+            scanner = new Scanner(System.in);
+            String title = scanner.nextLine();
+            System.out.print("Body: ");
+            String body = scanner.nextLine();
+
+            messages[indexMessage++] = new Message(title, body, onlineUser, announcement);
+            System.out.println("Message sent!");
+        } else {
+            System.out.println("There is no announcement!");
+        }
+    }
+
+    private static void viewInbox(Message[] messages) {
+        int index = 1;
+        System.out.println("All Messages");
+        System.out.println("-----------------------------------------");
+        for (Message message : messages) {
+            if (message != null) {
+                if (message.getAnnouncement().getUser().equals(onlineUser)) {
+                    System.out.println(index + ". " + message.toString());
+                    index++;
+                }
+            }
+        }
+        if (index == 1)
+            System.out.println("No data!");
+        System.out.println("-----------------------------------------");
+    }
+
+
+
+
+
+
+
+
+
 
 }
+
+
+
+
+
+
+
+
